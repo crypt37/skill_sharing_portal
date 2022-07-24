86c93ecfb7dc51097c4b09f852a9b3ae983de7d9
sujan bhattarai <sjnbhttr123@gmail.com>
2022-07-24 17:25:54 +0545

    still port error :(

--- test.js
+++ test.js
@@ -409,8 +409,10 @@ app.get("/logout", function(req, res) {
 // });
 
 
-app.listen(process.env.PORT ||5000, function () {
-    console.log("listening");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
 app.post('/data', userExists, (req, res, next) => {
